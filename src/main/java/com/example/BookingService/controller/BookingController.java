6bce7f3c8e6451937eca59e2feae83e344ff8c38
avinashLam <101968733+avinashLam@users.noreply.github.com>
2025-04-18 11:12:24 +0530

    Implemented Swagger ApI open documentation

--- src/main/java/com/example/BookingService/controller/BookingController.java
+++ src/main/java/com/example/BookingService/controller/BookingController.java
@@ -2,11 +2,17 @@ package com.example.BookingService.controller;
 
 import com.example.BookingService.entity.Booking;
 import com.example.BookingService.service.BookingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/booking")
@@ -21,20 +27,59 @@ public class BookingController {
 
 
     @PostMapping("/save")
+    @Operation(summary = "create a new booking")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully added",
+                    content = @Content(mediaType = "application/json")
+            ), @ApiResponse(
+            responseCode = "404",
+            description = "failed",
+            content = @Content
+    )
+    })
     public ResponseEntity<Booking> newBooking(@RequestBody Booking booking){
         Booking booking1=bookingService.saveBooking(booking);
         logger.info("Posting data");
         return  ResponseEntity.status(200).body(booking1);
     }
     @DeleteMapping("/{id}")
+    @Operation(summary="Delete By Id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "deleted successfully",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "failed",
+                    content = @Content
+            )
+    })
+
     public ResponseEntity<String> deletebyId(@PathVariable Integer id){
         String response=bookingService.deleteBooking(id);
         logger.info("deleting the data by Id "+ id);
 
-        return ResponseEntity.status(204).body("deleted");
+        return ResponseEntity.status(200).body("deleted");
     }
 
     @GetMapping("/{id}")
+    @Operation(summary="Get By Id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Get By id",
+                    content=@Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "failed",
+                    content = @Content
+            )
+    })
     public ResponseEntity<Booking> getBookingById(@PathVariable Integer id){
         Booking booking=bookingService.getBooking(id);
         logger.info("getting the data by Id");
@@ -43,13 +88,27 @@ public class BookingController {
         return ResponseEntity.status(200).body(booking);
     }
 
+
     @PutMapping("/{id}")
+    @Operation(summary="Update By Id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "update by Id",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "failed",
+                    content = @Content
+            )
+    })
     public ResponseEntity<Booking>  updatingBooking(@PathVariable Integer id,@RequestBody Booking booking){
         Booking booking1= bookingService.updateBooking(id,booking);
         logger.info("update the data by Id");
         logger.info("data  "+booking1.getBookingId());
 
-        return ResponseEntity.status(201).body(booking1);
+        return ResponseEntity.status(200).body(booking1);
     }
 
 }
